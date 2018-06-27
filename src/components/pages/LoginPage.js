6b9ce37c3dd543b95b9e387f1bfab5d65beaff5c
StarAzure <info.starazure@gmail.com>
2018-06-27 19:52:40 -0400

    Login Works

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import LoginForm from "../forms/LoginForm";
 
-const HomePage = () => <div>
-<h1>Login Page </h1>
+class LoginPage extends React.Component 
 
-<Link to="/login">Login</Link>
-</div>
+{
+  submit = data => {
+  	console.log (data)
+  };
+   
 
-export default HomePage;
+  render() {
+    return (
+      <div>
+        <h1>Login page</h1>
+
+        <LoginForm submit={this.submit} />
+
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
